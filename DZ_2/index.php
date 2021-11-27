217fab1cf6fad2484ae61ac22f1121bbc28222ec
Микола Матіос <mikolka09@gmail.com>
2021-11-27 18:02:08 +0100

    27.11.2021

---
+++ DZ_2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once ("lib/buildProductItem.php");
+
+
+$pageTitle = " Я принял решение - заголовок будет таким";
+
+require_once ("template/header.php");
+
+
+$openPage = "index.php";
+if (isset($_GET['openPage'])) $openPage = $_GET['openPage'];
+if (isset($_POST['openPage'])) $openPage = $_POST['openPage'];
+require_once ("pages/" . $openPage);
+
+
+require_once ("template/footer.php");
