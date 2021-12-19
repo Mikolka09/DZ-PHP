9026fb77e15e0c96759b8fb30ce97c382b39ee2c
Микола Матіос <mikolka09@gmail.com>
2021-12-19 17:18:40 +0100

    19.12.2021

--- DZ_13.12.2021/example-app/resources/views/dashboard.blade.php
+++ DZ_13.12.2021/example-app/resources/views/dashboard.blade.php
@@ -11,6 +11,11 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     You're logged in!
                 </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Показать список пользователей -
+                    <a href="userstable" class="btn btn-success align-right">Users</a>
+                </div>
+
             </div>
         </div>
     </div>
